ef208ea7066ffcb514ceafe9c182592495763a1b
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-05 19:47:32 +0100

    links and sidebar

---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import styles from './menuLink.module.css'
+
+const MenuLink = ({ item }) => {
+   const { title, path, icon } = item;
+   const pathname = usePathname();
+  return (
+    <Link href={path} 
+      className={`${styles.container} ${pathname === path && styles.active}`}>
+      {icon} {title}
+    </Link>
+  )
+}
+
+export default MenuLink
