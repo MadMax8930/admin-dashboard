ecdaf0042029a4457510720b03d301a89feb98be
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-07 15:23:17 +0100

    db update

--- app/database/connect.js
+++ app/database/connect.js
@@ -1,18 +1,17 @@
 import mongoose from 'mongoose'
 
 export const connectToDB = async () => {
+   let connection = {};
+
    try {
-      if (mongoose.connection.readyState === 1) {
+      if (mongoose.connections[0].readyState === 1) {
          console.log('Already connected to the database');
          return;
       }
 
-      await mongoose.connect(process.env.MONGODB_URI, {
-         useNewUrlParser: true,
-         useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.MONGODB_URI);
 
-      connection.isConnected = mongoose.connection.readyState;
+      connection.isConnected = mongoose.connections[0].readyState;
       console.log("Connection established to the database");
    } catch (error) {
       console.error('Error connecting to the database:', error.message);
